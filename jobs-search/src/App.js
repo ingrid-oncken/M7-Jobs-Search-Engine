a86fa9ba5429f948d614fbf07176b331615c8984
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-09-14 08:11:26 +0100

    add

--- jobs-search/src/App.js
+++ jobs-search/src/App.js
@@ -1,14 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
 import "./App.css"
 import MainNavbar from "./components/MainNavbar"
 import Search from "./components/Search"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route } from "react-router-dom"
+import Details from "./components/Details"
 
 function App() {
   return (
-    <div>
+    <Router>
       <MainNavbar />
-      <Search />
-    </div>
+      <Container>
+        <Search />
+        <Route path="/" exact component={Details} />
+      </Container>
+    </Router>
   )
 }
 
--- jobs-search/src/components/Search.jsx
+++ jobs-search/src/components/Search.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Form, FormControl } from "react-bootstrap"
+import { Button, Form, FormControl } from "react-bootstrap"
 import { useState, useEffect } from "react"
 
 const Search = () => {
@@ -11,10 +11,11 @@ const Search = () => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        `https://remotive.io/api/remote-jobs?search=front%20end`
+        `https://strive-jobs-api.herokuapp.com/jobs?title=developer`
       )
       if (response.ok) {
-        let data = await response.json()
+        let { data } = await response.json()
+
         console.log(data)
       }
     } catch (error) {
@@ -22,7 +23,7 @@ const Search = () => {
     }
   }
   return (
-    <Container>
+    <>
       <Form inline className="justify-content-center mt-5" size="sm">
         <FormControl
           type="text"
@@ -33,7 +34,7 @@ const Search = () => {
         />
         <Button variant="outline-info">Search</Button>
       </Form>
-    </Container>
+    </>
   )
 }
 export default Search
